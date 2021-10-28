046b0f44b1e55f7994a405aaafd7594253dcb0fc
oleider Jesus Gonzalez Vetancourt <69883350+argenismahath@users.noreply.github.com>
2021-10-28 06:30:17 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -11,11 +11,13 @@ let last2 = '';
 
 let lastbtn = '';
 let counter = 0;
-let open =false;
+//set varible (open) to difine if a list is open or close
+let open = false;
+let ultimo = document.querySelector('#' + last2 + '_m');
 
 container.addEventListener('click', (e) => {
 
-    let now= e.target.id;
+    let now = e.target.id;
     let show = e.target.id + '_m';
     let btn = e.target.id + '-btn';
 
@@ -23,72 +25,70 @@ container.addEventListener('click', (e) => {
     let listShow = document.querySelector('#' + show);
     let listbtn = document.querySelector('#' + btn);
 
-    console.log(e);
+    if (e.target.id == 'alitas' || 'costillas' || 'hamburguesa' || 'burrito' || 'taco' || 'jocho' || 'nacho' || 'bebida') {
 
-    if (e.target.id == 'alitas' || 'costillas' || 'hamburguesa' || 'burrito' || 'taco' || 'jocho' || 'nacho'  || 'bebida') {
-        open=true;
-        
         //show and hide main lists
         try {
-            if (listShow.style.display == 'none') {
-    
-                listShow.style.display = 'block';
-    
-            } else if (listShow.style.display == 'flex') {
-    
-                listShow.style.display = 'block';
-    
-            } else if (listShow.style.display == 'block') {
-                listShow.style.display = 'none';
-            } else {
-                listShow.style.display = 'block';
+            if (last != listShow) {
+
+                if (listShow.style.display == 'none') {
+
+                    listShow.style.display = 'block';
+                    open = true;
+
+                } else if (listShow.style.display == 'flex') {
+
+                    listShow.style.display = 'block';
+                    open = true;
+
+                } else if (listShow.style.display == 'block') {
+                    listShow.style.display = 'none';
+                    open = false;
+                } else {
+                    listShow.style.display = 'block';
+                    open = true;
+                }
             }
         }
         catch (e) { }
-        
+
         //rotate icon menu
-        try {
-            if (counter === 0) {
-    
-                listbtn.classList.add('btn-rotate');
-    
-                counter++;
-            } else if (counter === 1) {
-                console.log(lastbtn);
-                listbtn.classList.add('btn-rotate');
-    
-            }
-    
-        } catch (e) {
-    
+
+        if (listShow.style.display == 'block') {
+
+            listbtn.classList.add('btn-rotate');
         }
-       
+
+        if (last != '') {
+
+            console.log(lastbtn + 'hhh');
+            let last3 = document.getElementById(lastbtn);
+            let lastsections = document.getElementById(last);
+
+            if (last != listShow) {
+
+                last3.classList.remove('btn-rotate');
+                console.log(last+'last');
+                lastsections.style.display='none';
+            }
+        }
+
     }
 
-    // close the lats list ope
+    // close the lats list OR open
     try {
-        if (last != '' && open==true ) {
-            let ultimo=document.querySelector('#'+last2);
-            ultimo.style.display = 'none';
-            lastbtn.classList.remove('btn-rotate');
-            
+        if (now == 'alitas' || now == 'costillas' || now == 'hamburguesa' || now == 'burrito' || now == 'taco' || now == 'jocho' || now == 'nacho' || now == 'bebida') {
+            last2 = e.target.id;
+            last = listShow.id;
+            lastbtn = listbtn.id;
+            console.log('FUNTION CLOSE OR OPEN');
 
         }
-    } catch (e) {console.log('error '+ e);}
+    } catch (e) { console.log('error ' + e); }
 
-
-
-
-
-    last2 = e.target.id;
-    last=listShow
-    lastbtn = listbtn;
-    
-    console.log('hola');
 
 });
 
-var put = 0;
 function put() {
 
     // chelaL.style.display='block'
@@ -98,3 +98,22 @@ function put() {
 
     // console.log(s);
 }
+
+let kya = false;
+let check = document.querySelector('#check');
+check.addEventListener("change", (e) => {
+    if (kya == false) {
+        kya = true;
+    } else {
+        kya = false
+    }
+
+
+    console.log(kya);
+});
+
+
+
+
+//DEV: OLEIDER JESUS GONZALEZ VETANCOURT
+//EMAIL:oleidergonzalez11@gmail.com
